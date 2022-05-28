608d617d6e232eccb136dfb17475b8ab2e23441f
seungha <frazzle124@gmail.com>
2022-05-28 14:18:04 +0900

    event

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -5,26 +5,20 @@ import styles from "./Hello.module.css";
 
 const Hello = () => {
     return (
-        // jsx는 return안에 하나의 태그만으로 구성이 되어있어야하므로 div등으로 감싸야한다. 
-    <div>
-        <p style = {
-            // 스타일 작성법 1: 인라인으로 작성 
-            // 객체처럼 작성해야한다.
-            {
-                color : "red",
-                marginBottom: "50px"
-            
-            }
-        }> hello </p>
-        <World></World>
-        <div className={styles.box}>Hello</div>
-    </div>
+        <div>
+            <h1>Hello</h1>
+            <button onClick = {showName}>show name</button>
+            <button onClick = {() => {
+                console.log("20")
+            }}>show age</button>
+            <input type = "text" onChange={(e) => console.log(e.target.value)}></input>
+
+        </div>
     );
+}
+// 미리 함수를 만들어 전달
+function showName(){
+    console.log("seung")
 }
 
 export default Hello;
-
-// 아래와 같이 사용해도 된다.
-// export default function Hello() {
-//     <p> hello </p>
-// }
