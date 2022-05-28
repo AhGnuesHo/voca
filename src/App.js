05d731707fdbe7e38473776dc434038906c2d864
seungha <frazzle124@gmail.com>
2022-05-28 21:27:25 +0900

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Header from "./component/Header"
 import DayList from "./component/DayList"
 import Day from "./component/Day"
-
+import EmptyPage from "./component/EmptyPage"
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {  
   return(
+    <BrowserRouter>
+    
     <div className="App">
-      <Header></Header>
-      <DayList/>
-      <Day></Day>
+      <Header/>
+      {/* Routes 외부 : 모든 페이지에 공통으로 노출 */}
+      {/* Routes 내부 :url에 따라 다른 페이지 노출 */}
+      <Routes>
+        <Route path="/" element ={ <DayList/>}/>
+        <Route path="/day/:day" element = {<Day/>}/>
+        <Route path="*" element = {<EmptyPage/>}/>
+      </Routes>
       </div>
+    </BrowserRouter>
 
   )
 }
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,27 @@
 import dummy from "../db/data.json";
+// url 정보
+import { useParams } from "react-router-dom";
 
 export default function Day() {
-    const day = 2;
-    const wordList = dummy.words.filter(word => {
-        return word.day === day
-    })
+  const day = useParams().day;
+  const wordList = dummy.words.filter((word) => {
+    return word.day === day;
+  });
   return (
-    <table>
-      <tbody>
-        {wordList.map((word) => {
-          return (
-            <tr key ={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <h2> DAY {day} </h2>
+      <table>
+        <tbody>
+          {wordList.map((word) => {
+            return (
+              <tr key={word.id}>
+                <td>{word.eng}</td>
+                <td>{word.kor}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
